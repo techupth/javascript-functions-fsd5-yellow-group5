9dabe3eaf6d9cd57070ae90516ba09a6cabb3e78
Gun <cobaltgun2324@gmail.com>
2023-11-29 13:26:45 +0700

    feat: add complete exercise

---
+++ test.js
@@ -0,0 +1,17 @@
+let calculator = { add :(a, b) => {
+    const result = a + b;
+    return result;
+  }
+   subtract: (a, b) => {
+    const result = a - b;
+    return result;
+  }
+   multiply : (a, b) => {
+    const result = a * b;
+    return result;
+  }
+   divide : (a, b) => {
+    const result = a / b;
+    return result;
+  }
+}
